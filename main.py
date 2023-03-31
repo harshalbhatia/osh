2132f85778326f576aec0e02605b196b543f7795
Harshal Bhatia <2299-harshal.bhatia@users.noreply.source.golabs.io>
2023-04-01 02:59:39 +0530

    break down api logic into plugin based system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import openai
+from models import models
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -8,24 +9,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_base = os.getenv("OPENAI_API_BASE")
 openai.api_version = os.getenv("OPENAI_API_VERSION")
 
+CURRENT_MODEL = models.get(os.getenv("CURRENT_MODEL"))
 DEPLOYMENT_ID = os.getenv("DEPLOYMENT_ID")
 
-response = openai.ChatCompletion.create(
-  deployment_id=DEPLOYMENT_ID,
-  messages=[
-    {
-      "role":
-      "system",
-      "content":
-      "You are a helpful assistant that helps users of Unix systems (linux/mac and the like) with their terminal execution issues. The user will provide input/error info and you're supposed to reply with suggestions to help them fix the problem. It could be alternate commands, missing flags, and the like. If you're not sure, suggest the user to read more about a particular term. Please suggest a correct command if you have one and then a brief explanation."
-    },
-    {
-      "role": "user",
-      "content": "input: \"rm temp\"; error: \" rm: temp: is a directory\""
-    },
-  ],
-  temperature=0,
-)
-
-print(response['choices'][0]['message']['content'])
-
+def callCurrentModelWithData(data):
+    # figure out currently configured model
+    # calls it in a generic manner
+    # returns the result
