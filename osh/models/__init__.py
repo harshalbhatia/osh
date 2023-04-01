624a7619674fa1626ee9264e5f3fe85247acf5a3
Harshal Bhatia <2299-harshal.bhatia@users.noreply.source.golabs.io>
2023-04-01 12:47:59 +0530

    dirty: add curie model

--- osh/models/__init__.py
+++ osh/models/__init__.py
@@ -1,5 +1,15 @@
-from .turbo import predict
+from . import turbo, curie
 
 models = {
-    'turbo': predict
+    'turbo': turbo.predict,
+    'curie': curie.predict
 }
+
+current_model_name = os.getenv('CURRENT_MODEL')
+
+current_model = models.get(current_model_name)
+
+if current_model:
+    current_model()
+else:
+    print(f'Invalid model name: {current_model_name}')
